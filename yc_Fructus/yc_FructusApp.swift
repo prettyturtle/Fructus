124ee1d7cecbf32434905b03d77d232e148119ad
prettyturtle <unifconti@gmail.com>
2023-08-17 23:07:05 +0900

    Initial Commit

---
+++ yc_Fructus/yc_FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  yc_FructusApp.swift
+//  yc_Fructus
+//
+//  Created by yc on 2023/08/17.
+//
+
+import SwiftUI
+
+@main
+struct yc_FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
